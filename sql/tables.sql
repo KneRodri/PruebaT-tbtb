62d5e1ade20f46338006f45dbb4563587e0d304e
Knelia <knelia1989@gmail.com>
2023-12-14 16:51:43 -0500

    Se ordenan las tablas y se modifican los nombres a ingles para mantener consistencia

--- sql/tables.sql
+++ sql/tables.sql
@@ -1,31 +1,32 @@
-CREATE TABLE User (
+CREATE TABLE TbUser (
     user_id INT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    user_name VARCHAR(100) NOT NULL,
     email VARCHAR(50) UNIQUE,
+    country VARCHAR(50),
     city VARCHAR(50),
     creation_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE Order_List (
+CREATE TABLE TbOrderList (
     order_id INT PRIMARY KEY,
     user_id INT,
     total_order DECIMAL(10, 2),
     date_order DATE,
-    FOREIGN KEY (user_id) REFERENCES User(user_id)
+    FOREIGN KEY (user_id) REFERENCES TbUser(user_id)
 );
 
-CREATE TABLE Products (
+CREATE TABLE TbProducts (
     product_id INT PRIMARY KEY,
     name_product VARCHAR(50) NOT NULL,
     price DECIMAL(8, 2) NOT NULL
 );
 
 /* Relación muchos a muchos entre 'Pedidos' y 'Productos' */
-CREATE TABLE Order_Products (
+CREATE TABLE TbOrderProducts (
     order_id INT,
     product_id INT,
     quantity INT,
     PRIMARY KEY (order_id, product_id),
-    FOREIGN KEY (order_id) REFERENCES Order_List(order_id),
-    FOREIGN KEY (product_id) REFERENCES Products(product_id)
+    FOREIGN KEY (order_id) REFERENCES TbOrderList(order_id),
+    FOREIGN KEY (product_id) REFERENCES TbProducts(product_id)
 );
--- sql/transactions.sql
+++ sql/transactions.sql
@@ -1,14 +1,14 @@
-INSERT INTO User (user_id, name, email, city) VALUES
-(1, 'Betzy Rodríguez', 'betzy@example.com', 'City A'),
-(2, 'Bolívar Franco', 'bolivar@example.com', 'City B')
-(3, 'Glau Rojas', 'glau@example.com', 'City C'),
-(4, 'Lourdes Rojas', 'lourdes@example.com', 'City D'),
-(5, 'Louvia Franco', 'louvia@example.com', 'City E')
-(6, 'Mara Rodríguez', 'mara@example.com', 'City F'),
-(7, 'Rosmer Franco', 'rosmer@example.com', 'City G'),
-(8, 'Veronica Castillo', 'vero@example.com', 'City H');
+INSERT INTO TbUser (user_id, user_name, email, country, city) VALUES
+(1, 'Betzy Rodríguez', 'betzy@example.com', 'Venezuela', 'Lechería'),
+(2, 'Bolívar Franco', 'bolivar@example.com', 'Ecuador', 'Quito'),
+(3, 'Glau Rojas', 'glau@example.com', 'Francia', 'Paris'),
+(4, 'Lourdes Rojas', 'lourdes@example.com', 'España', 'Barcelona'),
+(5, 'Louvia Franco', 'louvia@example.com', 'Mexico', 'Cancún'),
+(6, 'Mara Rodríguez', 'mara@example.com', 'Argentina', 'Lanus'),
+(7, 'Rosmer Franco', 'rosmer@example.com', 'Venezuela', 'Puerto la cruz'),
+(8, 'Veronica Castillo', 'vero@example.com', 'Colombia', 'Bogotá');
 
-INSERT INTO Order_List (order_id, user_id, total_order, date_order) VALUES
+INSERT INTO TbOrderList (order_id, user_id, total_order, date_order) VALUES
 (101, 1, 120.50, '2023-12-13'),
 (102, 2, 72.50, '2023-12-13'),
 (103, 3, 80.10, '2023-12-13'),
@@ -18,15 +18,24 @@ INSERT INTO Order_List (order_id, user_id, total_order, date_order) VALUES
 (107, 7, 20.30, '2023-12-14'),
 (108, 8, 60.70, '2023-12-14');
 
-INSERT INTO Products (product_id, name_product, price) VALUES
+INSERT INTO TbProducts (product_id, name_product, price) VALUES
 (501, 'Producto A', 50.30),
 (502, 'Producto B', 60.40),
 (503, 'Producto C', 40.20),
 (504, 'Producto D', 20.50),
-(505, 'Producto E', 80.20);
+(505, 'Producto E', 80.20),
+(506, 'Producto F', 50.60);
 
-INSERT INTO Order_Products (order_id, product_id, quantity) VALUES
+INSERT INTO TbOrderProducts (order_id, product_id, quantity) VALUES
 (101, 501, 2),
 (103, 503, 1),
 (104, 504, 3),
 (105, 505, 1);
+
+INSERT INTO TbOrderProducts (order_id, product_id, quantity) VALUES
+(106, 504, 4),
+(107, 505, 1),
+(108, 506, 2);
+
+
+
