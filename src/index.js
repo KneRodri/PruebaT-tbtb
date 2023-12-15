b4ea88ffafac8a4b43cadd8d5a26e0607fc6d19f
Knelia <knelia1989@gmail.com>
2023-12-14 23:59:12 -0500

    Se crea base de datos, se agrega tabla de prueba, se prueba y la terminal indica que funciona, se agrega el escuchador en el puerto

--- src/index.js
+++ src/index.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const sqilite3 = require('sqlite3').verbose();   
 
 const app = express();
-const port = process.env.PORT || 9000;
+const port = process.env.PORT || 8000;
 
 app.use(express.json());
 
-let register = [
-    { id: 1, name: 'Name1', atri}
-]
+const db = new sqilite3.Database('my_database.db');
+
+const sqlScript = `
+  CREATE TABLE IF NOT EXISTS TbOrderStatus (
+   status_id INT PRIMARY KEY,
+   order_id INT,
+   status VARCHAR(50) NOT NULL,
+   status_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+   FOREIGN KEY (order_id) REFERENCES TbOrderList(order_id)
+)`;
+
+db.exec(sqlScript, (err) => {
+    if (err) {
+        console.error('Error executing SQL script:', err);
+    } else {
+        console.log('Successfully created tables');
+    }
+});
+
+
+
+app.listen(port, () => {
+    console.log('Server started on port', port);
+});
