15dbf728aec297090f1cf191b3968d48557a54ff
Knelia <knelia1989@gmail.com>
2023-12-15 18:38:30 -0500

    Se desinstala datatable, se agregantabla y renderiza información

--- src/lib/main.js
+++ src/lib/main.js
@@ -7,30 +7,51 @@ document.addEventListener("DOMContentLoaded", () => {
             return res.json();
        })
        .then(data => {
-            console.log(data);
             fillTable(data);
        })
        .catch(err => {
         console.error('There was a problem with the app:', err);
        });
-       const fillTable = (data) => {
+
+    const fillTable = (filteredData) => {
         const tableBody = document.querySelector('#myTable tbody');
+        const searchInput = document.getElementById('#searchInput');
+
+        const renderTable = (filteredData) => {
+            tableBody.innerHTML = '';
+
+            filteredData.forEach(user => {
+                const row = tableBody.insertRow();
+                row.innerHTML = `
+                    <td>${user.id}</td>
+                    <td>${user.name}</td
+                    <td>${user.username}</td>
+                    <td>${user.email}<td>
+                    <td>${user.address.city}, ${user.address.street}, ${user.address.suite}</td>
+                    <td>${user.phone}</td>
+                    <td>${user.website}</td>
+                    <td>${user.company.name}</td>
+                `;
+            });
+        }
+        renderTable(filteredData);
+
+        searchInput.addEventListener('keyup', () => {
+            const searchItem = searchInput.value;
+            const filteredData = data.filter(user => {
+                const userData = `${user.name} ${user.email} ${user.address.city}`;
+                return userData.includes(searchItem);
+            });
     
-        data.forEach(user => {
-            const row = tableBody.insertRow();
-            row.innerHTML = `
-                <td>${user.id}</td>
-                <td>${user.name}</td
-                <td>${user.username}</td>
-                <td>${user.email}<td>
-                <td>${user.address.city}, ${user.address.street}, ${user.address.suite}</td>
-                <td>${user.phone}</td>
-                <td>${user.website}</td>
-                <td>${user.company.name}</td>
-            `;
-        });
-        $('#myTable').DataTable();
+        renderTable(filteredData);
+    });
     }
 });
 
+// const fillTable = (data) => {
+//     const tableBody = document.querySelector('#myTable tbody');
 
+//     data.forEach(user => {
+
+//     });
+//     $('#myTable').DataTable();
