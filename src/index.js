ac5d13b9b26475b700601d3fcdb788b3824f3fac
Knelia <knelia1989@gmail.com>
2023-12-14 22:59:53 -0500

    Se installa dependencia sqlite3 para la base de datos

---
+++ src/index.js
@@ -0,0 +1,10 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 9000;
+
+app.use(express.json());
+
+let register = [
+    { id: 1, name: 'Name1', atri}
+]
